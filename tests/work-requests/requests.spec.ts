9583204d4704c325fd962da8abdca578cff16125
Alexander Vostretsov <alexandervostretsov@Alexanders-MacBook-Pro.local>
2024-12-04 17:51:22 -0800

    removed duplicate code in specs

--- tests/work-requests/requests.spec.ts
+++ tests/work-requests/requests.spec.ts
@@ -23,16 +23,10 @@ test.describe('Work Requests should include', () => {
 		await leftNav.selectProject('Work Requests')
 		await expect(workRequestsPage.heading).toBeVisible()
 	})
-	test(`"${scenarios[0].task}" item in "${scenarios[0].columnName}" column with "${scenarios[0].tags.join(', ')}" tags`, async ({ table }) => {
-		await table.verifyColumnItem(scenarios[0].columnName, scenarios[0].task)
-		await table.verifyItemTags(scenarios[0].task, scenarios[0].tags)
-	})
-	test(`"${scenarios[1].task}" item in "${scenarios[1].columnName}" column with "${scenarios[1].tags.join(', ')}" tags`, async ({ table }) => {
-		await table.verifyColumnItem(scenarios[1].columnName, scenarios[1].task)
-		await table.verifyItemTags(scenarios[1].task, scenarios[1].tags)
-	})
-	test(`"${scenarios[2].task}" item in "${scenarios[2].columnName}" column with "${scenarios[2].tags.join(', ')}" tags`, async ({ table }) => {
-		await table.verifyColumnItem(scenarios[2].columnName, scenarios[2].task)
-		await table.verifyItemTags(scenarios[2].task, scenarios[2].tags)
+	scenarios.forEach(scenario => {
+		test(`"${scenario.task}" item in "${scenario.columnName}" column with "${scenario.tags.join(', ')}" tags`, async ({ table }) => {
+			await table.verifyColumnItem(scenario.columnName, scenario.task)
+			await table.verifyItemTags(scenario.task, scenario.tags)
+		})
 	})
 })
